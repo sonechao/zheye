2fb9cf7a29a812456c8abac83c1858a7e85d7cd7
sonechao <sconghao@163.com>
2022-04-08 18:24:05 +0800

    feat: add ColumnList component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: 'test1的专栏的描述，有一段很有意思的内容，啦啦啦',
+    avatar: 'https://images.dog.ceo/breeds/germanshepherd/n02106662_7238.jpg'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: 'test1的专栏的描述，有一段很有意思的内容，啦啦啦',
+    avatar: 'https://images.dog.ceo/breeds/germanshepherd/n02106662_7238.jpg'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: 'test1的专栏的描述，有一段很有意思的内容，啦啦啦',
+    avatar: 'https://images.dog.ceo/breeds/germanshepherd/n02106662_7238.jpg'
+  }
+]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <div v-for="column in list" :key="column.id" class="col-4">
+      <img :src="column.avatar" :alt="column.title">
+      <h5>{{column.title}}</h5>
+      <p>{{column.description}}</p>
+      <a href="#">进入专栏</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps {
+  id: number;
+  title: string;
+  description: string;
+  avatar: string;
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      requireed: true
+    }
+  }
+  // setup(props) {
+  //   props.list[0].
+  // },
+})
+</script>
